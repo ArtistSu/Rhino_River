616640ab44834aa20990777545b7e927ddb9ca07
ArtistS-win <sylgg0918@gmail.com>
2025-07-04 00:45:54 +0100

    init & navbar

---
+++ rhino_river/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
---
+++ rhino_river/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Logo from "../../assets/1.png";
+
+const NavbarLinks = [
+  {
+    id: 1,
+    title: "Home",
+    line: "/",
+  },
+  {
+    id: 2,
+    title: "Features",
+    line: "#",
+  },
+  {
+    id: 3,
+    title: "Shop",
+    line: "#",
+  },
+  {
+    id: 4,
+    title: "About us",
+    line: "#",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    line: "#",
+  },
+];
+const Navbar = () => {
+  return (
+    <>
+      <div className="custom-container flex justify-between items-center">
+        {/* Logo Section */}
+        <div className="flex items-center gap-1">
+          <img src={Logo} alt="" className="w-[50px]" />
+          <p className="font-bold text-xl"> RR </p>
+        </div>
+        {/* Link Section */}
+        <div className="hidden md:block">
+          <ul className="flex gap-3 xl:gap-4">
+            {NavbarLinks.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a
+                    className="hover:text-primary uppercase text-sm md:text-base"
+                    herf={link.link}
+                  >
+                    {link.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* Button Section */}
+        <div>
+          <button className="primary-btn">Request For Quotes</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
